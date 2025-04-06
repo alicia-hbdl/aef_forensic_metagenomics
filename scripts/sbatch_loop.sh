2e0322c78f7aebee91f2ec786d569deb5cba84ad
Alicia Hobdell <aliciahobdell@Alicias-MacBook-Air.local>
2025-04-06 20:52:45 +0100

    add database to bash loop

--- scripts/sbatch_loop.sh
+++ scripts/sbatch_loop.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-databases=(db1 db2 db3)
+databases=(
+  /scratch/grp/msc_appbio/recovered/k2_standard_20241228
+  /scratch/grp/msc_appbio/recovered/core_nt_database
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_standard_08gb_20241228
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_pluspfp_16gb_20241228
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_pluspfp_08gb_20241228
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_standard_16gb_20241228
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_eupathdb48_20230407
+  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/custom
+)
+
 thresholds=(0 5 10 20 50 100 500)
 min_hits=(1 2 3 4 5)
 read_lengths=(75 100 150)
