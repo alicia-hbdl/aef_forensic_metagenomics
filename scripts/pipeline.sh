6af17a51085b67a04f6e8937c6d311b6d01e588a
Alicia Hobdell <k24087895@erc-hpc-login1.create.kcl.ac.uk>
2025-05-03 12:34:12 +0100

    update late night runs

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -232,7 +232,7 @@ for R1 in "$TRIMMED_DIR"/paired/*_R1_paired.fastq.gz; do
     # Taxonomic classification with Kraken2
     echo -e "\nClassifying metagenomic reads with Kraken2..."
     KRAKEN_CMD="kraken2 --d \"$DATABASE\" --threads 8 --report \"$REPORTS_DIR/${base}.k2report\" \
-               --report-minimizer-data --paired --minimum-hit-groups 2 \
+               --memory-mapping --report-minimizer-data --paired --minimum-hit-groups 2 \
                --classified-out \"$CLASSIFIED_DIR/${base}_classified#.fastq\" --unclassified-out \"$UNCLASSIFIED_DIR/${base}_unclassified#.fastq\" \
                \"$FILTERED_FASTQ_DIR/${base}_metagenomic.1\" \"$FILTERED_FASTQ_DIR/${base}_metagenomic.2\" \
                --output \"$KRAKEN2_DIR/${base}.kraken2\" --use-names 2>&1"
