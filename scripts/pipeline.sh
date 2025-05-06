906671881e97f208891ee309560f66f0e9163766
Alicia Hobdell <k24087895@erc-hpc-login1.create.kcl.ac.uk>
2025-05-06 12:43:21 +0100

    fix kraken2 command so it doesn't cause seg dump

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -231,13 +231,12 @@ for R1 in "$TRIMMED_DIR"/paired/*_R1_paired.fastq.gz; do
 
     # Taxonomic classification with Kraken2
     echo -e "\nClassifying metagenomic reads with Kraken2..."
-    KRAKEN_CMD="kraken2 --d \"$DATABASE\" --threads 8 --report \"$REPORTS_DIR/${base}.k2report\" \
-               --memory-mapping --report-minimizer-data --paired --minimum-hit-groups 2 \
-               --classified-out \"$CLASSIFIED_DIR/${base}_classified#.fastq\" --unclassified-out \"$UNCLASSIFIED_DIR/${base}_unclassified#.fastq\" \
-               \"$FILTERED_FASTQ_DIR/${base}_metagenomic.1\" \"$FILTERED_FASTQ_DIR/${base}_metagenomic.2\" \
-               --output \"$KRAKEN2_DIR/${base}.kraken2\" --use-names 2>&1"
+    KRAKEN_CMD="kraken2 --db \"$DATABASE\" --threads 8 --report \"$REPORTS_DIR/${base}.k2report\" \
+		--report-minimizer-data --paired --minimum-hit-groups 2 \
+		--classified-out \"$CLASSIFIED_DIR/${base}_classified#.fastq\" --unclassified-out \"$UNCLASSIFIED_DIR/${base}_unclassified#.fastq\" \
+		--output \"$KRAKEN2_DIR/${base}.kraken2\" --use-names \"$FILTERED_FASTQ_DIR/${base}_metagenomic.1\" \"$FILTERED_FASTQ_DIR/${base}_metagenomic.2\" 2>&1"
     echo "$KRAKEN_CMD"
-    eval "$KRAKEN_CMD"  
+    eval "$KRAKEN_CMD"
     echo "✅ Classification complete."
 
     # Abundance estimation with Bracken
