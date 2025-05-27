7c1798d6536a0c80c9a6eba9eab706c532271c1c
Alicia Hobdell <aliciahobdell@Alicias-MacBook-Air.local>
2025-05-27 16:02:19 +0100

    update scripts

--- scripts/sbatch_loop.sh
+++ scripts/sbatch_loop.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+DB_PATH="/scratch/users/k24087895/final_project/data/databases"
+
 databases=(
-  /scratch/grp/msc_appbio/recovered/k2_standard_20241228
-  /scratch/grp/msc_appbio/recovered/core_nt_database
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_standard_08gb_20241228
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_pluspfp_16gb_20241228
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_pluspfp_08gb_20241228
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_standard_16gb_20241228
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/k2_eupathdb48_20230407
-  /scratch/users/k24087895/final_project/metagenome_analysis/data/databases/custom
+  "$DB_PATH/k2_eupathdb48_20230407"
+  "$DB_PATH/k2_housepets_250510"
+  "$DB_PATH/k2_pluspfp_08gb_20241228"
+  "$DB_PATH/k2_pluspfp_16gb_20250402"
+  "$DB_PATH/k2_standard_08gb_20241228"
+  "$DB_PATH/k2_standard_16gb_20250402"
+  "$DB_PATH/k2_zymobiomics_250509"
 )
 
-thresholds=(0 5 10 20 50 100 500)
-min_hits=(1 2 3 4 5)
-read_lengths=(75 100 150)
+# Run a single pipeline job with trimming and host DNA removal enabled before looping over all databases
+jid_first=$(sbatch --parsable pipeline.sh \
+  -f /scratch/users/k24087895/final_project/zymobiomics_folder/raw_data \
+  -d "$DB_PATH/k2_standard_16gb_20250402" \
+  -t -r -g /scratch/users/k24087895/final_project/zymobiomics_folder/raw_data/ground_truth.csv)
 
+# Wait for the first job to finish
+while squeue -j "$jid_first" >/dev/null 2>&1; do
+    sleep 60
+done
+
+# Loop over all databases
 for db in "${databases[@]}"; do
-  for th in "${thresholds[@]}"; do
-    for mh in "${min_hits[@]}"; do
-      for len in "${read_lengths[@]}"; do
-        jid=$(sbatch --parsable pipeline.sh -fq reads -db "$db" -t "$th" -m "$mh" -l "$len")
-        while squeue -j "$jid" >/dev/null 2>&1; do sleep 60; done  # check every 1 minute
-      done
+    jid=$(sbatch --parsable pipeline.sh \
+      -f /scratch/users/k24087895/final_project/zymobiomics_folder/raw_data \
+      -d "$db" -g /scratch/users/k24087895/final_project/zymobiomics_folder/raw_data/ground_truth.csv)
+    
+    # Wait for the submitted job to complete before submitting the next
+    while squeue -j "$jid" >/dev/null 2>&1; do
+        sleep 60
     done
-  done
 done
