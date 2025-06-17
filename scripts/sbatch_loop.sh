edc138791158af9abb2978f0c3e8ae3b3d03b39e
Alicia Hobdell <k24087895@erc-hpc-comp026.create.kcl.ac.uk>
2025-06-17 18:29:31 +0100

    update runs with core_nt

--- scripts/sbatch_loop.sh
+++ scripts/sbatch_loop.sh
@@ -3,15 +3,15 @@
 DB_PATH="/scratch/prj/aef_forensic_metagenomics/data/databases"
 
 databases=(
-#"$DB_PATH/k2_core_nt_20240904"
-"$DB_PATH/k2_housepets_250510"
-"$DB_PATH/k2_pluspfp_16gb_20250402"
+"$DB_PATH/k2_core_nt_20240904"
+#"$DB_PATH/k2_housepets_250510"
+#"$DB_PATH/k2_pluspfp_16gb_20250402"
 #"$DB_PATH/k2_standard_08gb_20241228"
 #"$DB_PATH/k2_standard_20250402"
 #"$DB_PATH/k2_eupathdb48_20230407"
-"$DB_PATH/k2_pluspfp_08gb_20241228"
-"$DB_PATH/k2_pluspfp_20250402"
-"$DB_PATH/k2_standard_16gb_20250402"
+#"$DB_PATH/k2_pluspfp_08gb_20241228"
+#"$DB_PATH/k2_pluspfp_20250402"
+#"$DB_PATH/k2_standard_16gb_20250402"
 #"$DB_PATH/k2_zymobiomics_250509"
 )
 
