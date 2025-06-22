5cea92d62d87701652e33c75fa27d514155fd437
Alicia Hobdell <k24087895@erc-hpc-login2.create.kcl.ac.uk>
2025-06-22 14:12:36 +0100

    update runs with threshold 5

--- scripts/sbatch_loop.sh
+++ scripts/sbatch_loop.sh
@@ -3,7 +3,7 @@
 DB_PATH="/scratch/prj/aef_forensic_metagenomics/data/databases"
 
 databases=(
-"$DB_PATH/k2_core_nt_20240904"
+#"$DB_PATH/k2_core_nt_20240904"
 #"$DB_PATH/k2_housepets_250510"
 #"$DB_PATH/k2_pluspfp_16gb_20250402"
 #"$DB_PATH/k2_standard_08gb_20241228"
@@ -15,19 +15,20 @@ databases=(
 #"$DB_PATH/k2_zymobiomics_250509"
 )
 
-K2_MIN_HIT_VALUES=(1 2 3 4)
+K2_MIN_HIT_VALUES=(5)
+#1 2 3 4)
 B_THRESHOLD_VALUES=(0 5 10 20 40)
 
 # Run a single pipeline job with trimming and host DNA removal enabled before looping over all databases
-jid_first=$(sbatch --parsable pipeline.sh \
-  -f /scratch/prj/aef_forensic_metagenomics/exposure_folder/raw_data \
-  -d "$DB_PATH/k2_standard_16gb_20250402" \
-  -t -r)
+#jid_first=$(sbatch --parsable pipeline.sh \
+ # -f /scratch/prj/aef_forensic_metagenomics/zymobiomics_folder/raw_data \
+  #-d "$DB_PATH/k2_standard_16gb_20250402" \
+  #-t -r)
 
 # Wait for the first job to finish
-while squeue -j "$jid_first" >/dev/null 2>&1; do
-    sleep 60
-done
+#while squeue -j "$jid_first" >/dev/null 2>&1; do
+ #   sleep 60
+#done
 
 # Loop through all database paths
 for db in "${databases[@]}"; do # Loop through all database paths
@@ -37,9 +38,9 @@ for db in "${databases[@]}"; do # Loop through all database paths
       echo "Submitting job for DB: $db | k2-min-hit: $k2_hit | b-threshold: $b_thresh"
       
       jid=$(sbatch --parsable pipeline.sh \
-        -f /scratch/prj/aef_forensic_metagenomics/exposure_folder/raw_data \
-        -d "$db" --k2-min-hit $k2_hit --b-threshold $b_thresh) #\
-        #-g /scratch/prj/aef_forensic_metagenomics/zymobiomics_folder/raw_data/ground_truth.csv)
+        -f /scratch/prj/aef_forensic_metagenomics/zymobiomics_folder/raw_data \
+        -d "$db" --k2-min-hit $k2_hit --b-threshold $b_thresh \
+        -g /scratch/prj/aef_forensic_metagenomics/zymobiomics_folder/raw_data/ground_truth.csv)
       
       # Wait for job completion before launching the next
       while squeue -j "$jid" >/dev/null 2>&1; do
